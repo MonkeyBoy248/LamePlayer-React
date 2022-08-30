2fa5a374b058e609ea2350fbb701399f92d10d97
Kirill Temnyi <kirik201100@mail.ru>
2022-08-30 17:51:06 +0300

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import BurgerButton from '../BurgerButton/BurgerButton';
+import styles from './Header.module.scss';
+import Logo from '../Logo';
+import '../../styles/_fonts.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.header__inner} _container`}>
+          <BurgerButton></BurgerButton>
+          <div className={styles.header__logoWrapper}>
+            <Logo width={'80'} height={'80'}></Logo>
+            <h1 className={styles.header__pageTitle}>LamePlayer</h1>
+          </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
