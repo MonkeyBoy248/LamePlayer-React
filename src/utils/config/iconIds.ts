19b3dff94bf5a3276a1b8d469192512d53f032c2
Kirill Temnyi <kirik201100@mail.ru>
2022-08-30 17:59:02 +0300

    feat: add iconIds object with all icon ids

---
+++ src/utils/config/iconIds.ts
@@ -0,0 +1,19 @@
+interface IconId {
+    [id: string]: string;
+}
+  
+export const iconIds: IconId = {
+    tracks: 'icon-tracks',
+    albums: 'icon-albums',
+    playlists: 'icon-playlists',
+    settings: 'icon-settings',
+    play: 'icon-play',
+    prev: 'icon-prev',
+    next: 'icon-next',
+    dots: 'icon-dots',
+    shuffle: 'icon-shuffle',
+    mute: 'icon-volume_mute',
+    mid: 'icon-volume_mid',
+    full: 'icon-volume_full',
+    repeat: 'icon-repeat'
+}
