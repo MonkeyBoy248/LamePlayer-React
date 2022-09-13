205e60a938d49a3bdab226a60122d1f0f8ae3cf7
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-13 07:56:37 +0300

    feat: add Recommendations component to the main page

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import styles from './MainContent.module.scss';
+import Recommendations from '../../pages/Recommendations';
 
 const MainContent = () => {
   return (
     <main className={styles.mainContent}>
       <div className={`${styles.mainContent__inner}`}>
         <Sidebar />
+        <Recommendations />
       </div>
     </main>
   )
