38dbc2efa79125fc9c1fe45c22df78ac264ea790
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-13 07:50:39 +0300

    feat: add file with mock tracks data

---
+++ src/services/mockDataService.ts
@@ -0,0 +1,151 @@
+import { Track } from "../interfaces/TrackInfo";
+
+export const mockTracks: Track[] = [
+  {
+      trackName: 'Judith',
+      artist: 'A Perfect Circle',
+      album: 'Mer de Noms',
+      cover: 'mer-de-noms',
+      trackSrc: 'A Perfect Circle - Judith'
+  },
+  {
+      trackName: 'The Outsider',
+      artist: 'A Perfect Circle',
+      album: 'Thirteenth Step',
+      cover: 'thirteenth-step',
+      trackSrc: 'A Perfect Circle - The Outsider'
+  },
+  {
+      trackName: 'Parasite Eve',
+      artist: 'Bring Me The Horizon',
+      album: 'Post Human: Survival Horror',
+      cover: 'post-human-survival-horror',
+      trackSrc: 'Bring Me The Horizon - Parasite Eve'
+  },
+  {
+      trackName: 'Teardrops',
+      artist: 'Bring Me The Horizon',
+      album: 'Post Human: Survival Horror',
+      cover: 'post-human-survival-horror',
+      trackSrc: 'Bring Me The Horizon - Teardrops'
+  },
+  {
+      trackName: 'Anna Molly',
+      artist: 'Incubus',
+      album: 'Light Grenades',
+      cover: 'light-grenades',
+      trackSrc: 'Incubus - Anna Molly'
+  },
+  {
+      trackName: 'Drive',
+      artist: 'Incubus',
+      album: 'Make Yourself',
+      cover: 'make-yourself',
+      trackSrc: 'Incubus - Drive'
+  },
+  {
+      trackName: 'Love Hurts',
+      artist: 'Incubus',
+      album: 'Light Grenades',
+      cover: 'light-grenades',
+      trackSrc: 'Incubus - Love Hurts'
+  },
+  {
+      trackName: 'This Love',
+      artist: 'Maroon 5',
+      album: 'Songs About Jane',
+      cover: 'songs-about-jane',
+      trackSrc: 'Maroon 5 - This Love'
+  },
+  {
+      trackName: 'Wake Up Call',
+      artist: 'Maroon 5',
+      album: "It Won`t Be Soon Before Long",
+      cover: 'wake-up-call',
+      trackSrc: 'Maroon 5 - Wake Up Call'
+  },
+  {
+      trackName: 'Carnival Of Rust',
+      artist: 'Poets Of The Fall',
+      album: 'Carnival Of Rust',
+      cover: 'carnival-of-rust',
+      trackSrc: 'Poets Of The Fall - Carnival Of Rust'
+  },
+  {
+      trackName: 'Given And Denied',
+      artist: 'Poets Of The Fall',
+      album: 'Twilight Theater',
+      cover: 'twilight-theater',
+      trackSrc: 'Poets Of The Fall - Given And Denied'
+  },
+  {
+      trackName: 'King Of Fools',
+      artist: 'Poets Of The Fall',
+      album: 'Carnival Of Rust',
+      cover: 'carnival-of-rust',
+      trackSrc: 'Poets Of The Fall - King Of Fools'
+  },
+  {
+      trackName: "Fur Cue",
+      artist: 'Seether',
+      album: 'Holding Onto Strings Better Left To Fray',
+      cover: 'holding-onto-strings-better-left-to-fray',
+      trackSrc: 'Seether - Fur Cue'
+  },
+  {
+      trackName: 'Simplest Mistake',
+      artist: 'Seether',
+      album: 'Karma And Effect',
+      cover: 'karma-and-effect',
+      trackSrc: 'Seether - Simplest Mistake'
+  },
+  {
+      trackName: "The Guardian (Ellie's Song)",
+      artist: 'Shawn James',
+      album: 'The Guardian',
+      cover: 'the-guardian',
+      trackSrc: "Shawn James - The Guardian (Ellie's Song)"
+  },
+  {
+      trackName: 'All The Same',
+      artist: 'Sick Puppies',
+      album: 'Dressed Up As Life',
+      cover: 'dressed-up-as-life',
+      trackSrc: 'Sick Puppies - All The Same'
+  },
+  {
+      trackName: "You're Going Down",
+      artist: 'Sick Puppies',
+      album: 'Tri-Polar',
+      cover: 'tri-polar',
+      trackSrc: "Sick Puppies - You're Going Down"
+  },
+  {
+      trackName: 'Vicarious',
+      artist: 'Tool',
+      album: '10, 000 Days',
+      cover: '10, 000-days',
+      trackSrc: 'Tool - Vicarious'
+  },
+  {
+      trackName: 'Schism',
+      artist: 'Tool',
+      album: 'Lateralus',
+      cover: 'lateralus',
+      trackSrc: 'Tool - Schism'
+  },
+  {
+      trackName: 'Parabola',
+      artist: 'Tool',
+      album: 'Lateralus',
+      cover: 'lateralus',
+      trackSrc: 'Tool - Parabola'
+  },
+  {
+      trackName: 'The Pot',
+      artist: 'Tool',
+      album: '10, 000 Days',
+      cover: '10, 000-days',
+      trackSrc: 'Tool - The Pot'
+  },
+]
