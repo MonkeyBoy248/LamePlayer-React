45d2d6daf8ac6e3048ae8f44880e0cfa22b8df35
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-18 23:18:38 +0300

    chore: change the trackInfo propertiy names

--- src/features/Recommendations/components/RecommendationCard/RecommendationCard.tsx
+++ src/features/Recommendations/components/RecommendationCard/RecommendationCard.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import Icon from "../../../../components/Icon";
-import { Track } from "../../../../interfaces/TrackInfo";
+import { TrackData } from "../../../../interfaces/Track";
 import { iconIds } from "../../../../utils/config/iconIds";
 import styles from './RecommendationCard.module.scss';
 
 interface RecommendationCardProps {
-  trackInfo: Track;
+  trackInfo: TrackData;
 }
 
 const RecommendationCard = ({trackInfo}: RecommendationCardProps) => {
   return (
-    <div className={styles.recommendationCard}>
-      <div className={styles.recommendationCard__inner} style={{backgroundImage: `url(/images/covers/${trackInfo.cover}.jpg)` }}>
+    <li className={styles.recommendationCard}>
+      <div className={styles.recommendationCard__inner} style={{backgroundImage: `url(/images/covers/${trackInfo.cover})` }}>
         <button className={styles.recommendationCard__playButton}>
           <Icon id={iconIds.play} width='2em' height='2em' blockName='recommendationCard' fill='#000000' />
         </button>
         <div className={styles.recommendationCard__trackInfoWrapper}>
           <div className={styles.recommendationCard__trackInfo}>
-            <p className={styles.recommendationCard__trackName}>{trackInfo.trackName}</p>
+            <p className={styles.recommendationCard__trackName}>{trackInfo.name}</p>
             <p className={styles.recommendationCard__artist}>{trackInfo.artist}</p>
             <p className={styles.recommendationCard__duration}></p>
           </div>
@@ -26,8 +26,8 @@ const RecommendationCard = ({trackInfo}: RecommendationCardProps) => {
           </div>
         </div>
       </div>
-      <audio src={trackInfo.trackSrc} controls={false}></audio>
-    </div>
+      <audio src={trackInfo.src} controls={false}></audio>
+    </li>
   )
 }
 
