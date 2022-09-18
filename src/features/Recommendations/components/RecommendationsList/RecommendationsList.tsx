509267072c175805f1c376b7607cc74a2a95a590
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-18 20:56:39 +0300

    refactor: add a key property to RecommendationCard

--- src/features/Recommendations/components/RecommendationsList/RecommendationsList.tsx
+++ src/features/Recommendations/components/RecommendationsList/RecommendationsList.tsx
@@ -3,14 +3,16 @@ import styles from './RecommendationsList.module.scss';
 import { mockTracks } from "../../../../services/mockDataService";
 import RecommendationCard from "../RecommendationCard/RecommendationCard";
 import { Track } from "../../../../interfaces/TrackInfo";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const RecommendationsList = () => {
-  const mockRecommendations = mockTracks.slice(9, 12);
+  const mockRecommendations = mockTracks.slice(12, 15);
 
   return (
     <div className={styles.recommendationsList}>
       {mockRecommendations.length > 0 && mockRecommendations.map((item: Track) => {
-        return <RecommendationCard trackInfo={item} />
+        return <RecommendationCard key={uuidv4()} trackInfo={item} />
       })}
     </div>
   )
