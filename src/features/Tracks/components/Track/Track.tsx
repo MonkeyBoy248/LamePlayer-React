f81f8faa9501bc088da5849e9f0cde41675d7a3d
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-20 07:48:21 +0300

    feat: add styles for Track component

--- src/features/Tracks/components/Track/Track.tsx
+++ src/features/Tracks/components/Track/Track.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from './Track.module.scss';
 import { Link } from 'react-router-dom';
 import { TrackData } from "../../../../interfaces/Track";
-import { title } from "process";
 import Icon from "../../../../components/Icon";
 import { iconIds } from "../../../../utils/config/iconIds";
 
@@ -15,21 +14,26 @@ const Track = ({ track }: TrackProps) => {
     <li className={styles.track}>
       <div className={styles.track__inner}>
         <div className={styles.track__trackInfoWrapper}>
-            <img className={styles.track} src={`/images/covers/${track.cover}`} alt={`${track.album} cover`} />
-            <div className={styles.track__trackInfo}>
-              <Link className={styles.track__name} to={`/track/:${track.id}`}>{track.name}</Link>
+          <div className={styles.track__coverWrapper}>
+            <button className={styles.track__playButton}>
+              <Icon id={iconIds.play} width="1.5em" height="1.5em" blockName="track"></Icon>
+            </button>
+            <img className={styles.track__cover} src={`/images/covers/${track.cover}`} alt={`${track.album} cover`} />
+          </div>
+          <div className={styles.track__trackInfo}>
+              <Link className={styles.track__name} to={`/track/${track.id}`}>{track.name}</Link>
               <p className={styles.track__band}>{track.artist}</p>
             </div>
-            <div className={styles.track__controlsWrapper}>
+          </div>
+          <div className={styles.track__controlsWrapper}>
             <button className={styles.track__addToFavoritesButton}>
-                <Icon id={iconIds.like} width='1.5em' height='1.5em' blockName='track' fill='#E5E5E5'></Icon>
-              </button>
-              <button className={styles.track__deleteButton}>
-                <Icon id={iconIds.delete} width='1.5em' height='1.5em' blockName='track' fill='#E5E5E5'></Icon>
-              </button>
-              <button className={styles.track__addToPlaylist}>+</button>
-              <p className={styles.track__duration}>0:00</p>
-            </div>
+              <Icon id={iconIds.like} width='1.5em' height='1.5em' blockName='track' fill='#E5E5E5'></Icon>
+            </button>
+            <button className={styles.track__deleteButton}>
+              <Icon id={iconIds.delete} width='1.5em' height='1.5em' blockName='track' fill='#E5E5E5'></Icon>
+            </button>
+            <button className={styles.track__addToPlaylist}>+</button>
+            <p className={styles.track__duration}>0:00</p>
           </div>
       </div>
     </li>
