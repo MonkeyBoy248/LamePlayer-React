dc7728f08efe4688b5c74cbf9e2d8d60f1b0eba5
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-25 22:26:52 +0300

    feat: add new routes

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -4,6 +4,8 @@ import styles from './MainContent.module.scss';
 import Home from '../../pages/Home/Home';
 import { Route, Routes } from 'react-router-dom'
 import Tracks from '../../pages/Tracks/Tracks';
+import PlaylistsPage from '../../pages/Playlists/PlaylistsPage';
+import { playlists, tracks } from '../../services/mockDataService';
 
 const MainContent = () => {
   return (
@@ -12,7 +14,9 @@ const MainContent = () => {
         <Sidebar />
         <Routes>
           <Route path='/' element={<Home title='Recommendations' />}></Route>
-          <Route path='/tracks' element={<Tracks title='Tracks'></Tracks>}></Route>
+          <Route path='/tracks' element={<Tracks title='Tracks' tracks={tracks} />}></Route>
+          <Route path='/playlists' element={<PlaylistsPage title='Playlists' />}></Route>
+          <Route path='/playlist/:id' element={<Tracks title={playlists[0].name} tracks={playlists[0].tracks} />}></Route>
         </Routes>
       </div>
     </main>
