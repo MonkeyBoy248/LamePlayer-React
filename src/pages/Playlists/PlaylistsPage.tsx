9a1ca89f3c13d0900ea39bfd2bde52e9a945934f
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-25 22:26:19 +0300

    feat: create PlaylistsPage component

---
+++ src/pages/Playlists/PlaylistsPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PlayLists from "../../features/Playlists/components/PlayLists/PlayLists";
+import { Page } from "../../interfaces/Page";
+import styles from './PlaylistsPage.module.scss';
+import { playlists } from "../../services/mockDataService";
+import { Route } from "react-router-dom";
+import Tracks from "../Tracks/Tracks";
+
+const PlaylistsPage = ({ title }: Page) => {
+  return (
+    <section className={`${styles.playlistsPage} _container`}>
+      <h2 className={`${styles.playlistsPage__title} _pageTitle`}>{title}</h2>
+      {playlists.length > 0
+      ?
+      <PlayLists playlists={playlists}></PlayLists>
+      :
+      <p>No playlist was found</p>
+    }
+    </section>
+  )
+}
+
+export default PlaylistsPage;
