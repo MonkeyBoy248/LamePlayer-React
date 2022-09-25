1e9757e13dcff1aeae61315648c1e17388edc208
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-25 22:33:07 +0300

    feat: create Playlist component

---
+++ src/features/Playlists/components/Playlist/Playlist.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from './Playlist.module.scss';
+import { Link } from 'react-router-dom';
+import { PlaylistModel } from "../../../../interfaces/Playlist";
+import { TrackModel } from "../../../../interfaces/Track";
+import Icon from "../../../../components/Icon";
+import { iconIds } from "../../../../utils/config/iconIds";
+
+interface PlaylistProps {
+  playlist: PlaylistModel;
+}
+
+const Playlist = ({ playlist }: PlaylistProps) => {
+  const blockName = 'playlist';
+  const getTracksAmount = (tracks: TrackModel[]) => {
+    const tracksAmount = tracks.length;
+
+    return tracksAmount > 1 ? `${tracksAmount} tracks` : `${tracksAmount} track` ;
+  }
+
+  return (
+    <li className={styles.playlist}>
+      <Link className={styles.playlist__inner} to={`/playlist/${playlist.id}`}>
+        <div className={styles.playlist__coverWrapper}>
+          <img className={styles.playlist__cover} src={`/images/covers/${playlist.cover_url}`} alt={`${playlist.name} cover`} />
+        </div>
+        <h3 className={styles.playlist__name}>{playlist.name}</h3>
+        <p className={styles.playlist__tracksAmount}>{getTracksAmount(playlist.tracks)}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default Playlist;
