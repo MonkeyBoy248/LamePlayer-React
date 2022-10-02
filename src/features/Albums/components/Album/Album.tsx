fb48b8cb11bc6a0b75b467e535c3b3b420754683
MonkeyBoy248 <kirik201100@mail.ru>
2022-10-02 20:50:07 +0300

    feat: add styles for Album component

--- src/features/Albums/components/Album/Album.tsx
+++ src/features/Albums/components/Album/Album.tsx
@@ -10,13 +10,15 @@ interface AlbumProps {
 const Album = ({ album }: AlbumProps) => {
   return (
     <li className={styles.album}>
-      <div className={styles.album__inner}>
-        <img src={`/images/covers/${album.cover}`} alt={`${album.title} cover`} />
+      <Link className={styles.album__inner} to={`/album/${album.id}`}>
+        <div className={styles.album__coverWrapper}>
+          <img className={styles.album__coverImage} src={`/images/covers/${album.cover}`} alt={`${album.title} cover`} />
+        </div>
         <div className={styles.album__infoWrapper}>
-          <Link className={styles.album__title} to={`album/${album.id}`}>{album.title}</Link>
+          <Link className={`${styles.album__title} _itemTitle`} to={`album/${album.id}`}>{album.title}</Link>
           <p className={styles.album__artist}>{album.artist}</p>
         </div>
-      </div>
+      </Link>
     </li>
   )
 }
