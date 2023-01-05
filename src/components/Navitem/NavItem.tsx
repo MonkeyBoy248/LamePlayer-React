0ab29cccf90c6ba493d422a49745a950b6b8f1ee
Kirill Temnyi <kirik201100@mail.ru>
2023-01-05 22:36:35 +0300

    feat: add styles for active link

--- src/components/Navitem/NavItem.tsx
+++ src/components/Navitem/NavItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationItem } from '@interfaces/NavigationItem';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Icon from '../Icon';
 import styles from './NavItem.module.scss';
 
@@ -8,17 +8,17 @@ export interface NavItemProps extends NavigationItem{}
 
 const NavItem = ({ title, svg }: NavItemProps) => {
   return (
-    <li className={`${styles.navItem}`}>
-      <Link to={`/${title.toLowerCase()}`}>
+    <li className={ `${styles.navItem}` }>
+      <NavLink to={ `/${title.toLowerCase()}` } className={ isActive => isActive ? 'navItem_active' : undefined }>
         <Icon
-            id={svg.id}
-            blockName={svg.blockName}
-            width={svg.width}
-            height={svg.height}
-            fill={svg.fill}
+            id={ svg.id }
+            blockName={ svg.blockName }
+            width={ svg.width }
+            height={ svg.height }
+            fill={ svg.fill }
             />
-          <p className={`${styles.navItem__name} _text`}>{title}</p>
-      </Link>
+          <p className={ `${styles.navItem__name} _text` }>{ title }</p>
+      </NavLink>
     </li>
   )
 }
