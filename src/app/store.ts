0cc3e9e0ba86a610ed2771290fb4bff9d7f97702
Kirill Temnyi <kirik201100@mail.ru>
2023-01-08 20:53:25 +0300

    chore: change indents
    LP-6

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import trackReducer from "@features/Tracks/trackSlice";
 
 export const store = configureStore(
-    {
-        reducer: {
-          track: trackReducer
-        },
-    }
+  {
+      reducer: {
+        track: trackReducer
+      },
+  }
 )
 
 export type RootState = ReturnType<typeof store.getState>;
