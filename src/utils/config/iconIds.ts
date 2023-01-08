f33b3be42069522ef6f7bf9927d712bf27ad9030
Kirill Temnyi <kirik201100@mail.ru>
2023-01-08 23:47:30 +0300

    feat: add pause icon
    LP-3

--- src/utils/config/iconIds.ts
+++ src/utils/config/iconIds.ts
@@ -1,13 +1,10 @@
-interface IconId {
-    [id: string]: string;
-}
-
-export const iconIds: IconId = {
+export const iconIds = {
     tracks: 'icon-tracks',
     albums: 'icon-albums',
     playlists: 'icon-playlists',
     settings: 'icon-settings',
     play: 'icon-play',
+    pause: 'icon-pause',
     prev: 'icon-prev',
     next: 'icon-next',
     dots: 'icon-dots',
@@ -19,4 +16,4 @@ export const iconIds: IconId = {
     artists: 'icon-artists',
     like: 'icon-like',
     delete: 'icon-delete'
-}
+};
