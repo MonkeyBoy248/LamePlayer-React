d24cd589658ac4966f5813144313f51b02280ef0
Kirill <kirik201100@mail.ru>
2023-01-13 20:25:10 +0300

    feat: add custom hook for event listeners
    LP-3

---
+++ src/utils/hooks/useEventListener.ts
@@ -0,0 +1,28 @@
+import { RefObject, useCallback, useEffect, useRef } from 'react';
+
+export const useEventListener = (
+  element :RefObject<Element> | Document | Window = window,
+  type: keyof WindowEventMap,
+  listener: EventListener,
+  ): void => {
+  const savedListener = useRef<EventListener>();
+
+  useEffect(() => {
+    savedListener.current = listener;
+  }, [listener]);
+
+  const handleEventListener = useCallback((event: Event) => {
+    savedListener.current?.(event);
+  }, []);
+
+  useEffect(() => {
+    if (!element && !element['addEventListener']) {
+      return;
+    }
+
+    const target = 'current' in element ? element.current : element;
+    target?.addEventListener(type, handleEventListener);
+
+    return () => target?.removeEventListener(type, handleEventListener);
+  }, [type, element, handleEventListener]);
+};
