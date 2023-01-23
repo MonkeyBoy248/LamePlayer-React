d8146e5dbdd69581d5e415e33e6838cf99a9b1db
Kirill <kirik201100@mail.ru>
2023-01-23 19:29:46 +0300

    feat: add click handler for play/pause button
    LP-8

--- src/features/Recommendations/components/RecommendationsList/RecommendationsList.tsx
+++ src/features/Recommendations/components/RecommendationsList/RecommendationsList.tsx
@@ -1,31 +1,52 @@
 import styles from './RecommendationsList.module.scss';
-import { getRecommendations, tracks } from "@services/mockDataService";
 import RecommendationCard from "../RecommendationCard/RecommendationCard";
 import { TrackModel } from "@interfaces/Track";
-import { useEffect, useState } from 'react';
+import { setCurrentTrackIndex, setIsPlaying } from '@/features/Tracks/trackSlice';
+import { AppDispatch, RootState } from '@/app/store';
+import { useDispatch, useSelector } from 'react-redux';
 
-const RecommendationsList = () => {
- const recommendations = useRecommendations();
+interface RecommendationsListProps {
+  recommendationTracks: TrackModel[];
+  trackList: TrackModel[];
+}
+
+const RecommendationsList = ({ recommendationTracks, trackList }: RecommendationsListProps) => {
+  const dispatch: AppDispatch = useDispatch();
+  const currentTrackIndex = useSelector((state: RootState) => state.tracks.currentTrackIndex);
+  const isPlaying = useSelector((state: RootState) => state.tracks.isPlaying);
+
+  const setCurrentTrack = (id: string) => {
+    const trackItemIndex = trackList.findIndex((track) => track.id === id);
+
+    if (currentTrackIndex === trackItemIndex) {
+      dispatch(setIsPlaying(!isPlaying));
+
+      return;
+    }
+
+    dispatch(setCurrentTrackIndex(trackItemIndex));
+  }
+
+  const isRecommendationItemActive = (item: TrackModel) => {
+    const trackItemIndex = trackList.findIndex((track) => track.id === item.id);
+
+    return currentTrackIndex === trackItemIndex;
+  }
 
   return (
     <ul className={styles.recommendationsList}>
-      {recommendations.length > 0 && recommendations.map((item: TrackModel) => {
-        return <RecommendationCard key={item.id} trackInfo={item} />
+      {recommendationTracks.length > 0 && recommendationTracks.map((item: TrackModel, index: number) => {
+        return <RecommendationCard
+          key={item.id}
+          isPlaying={isPlaying}
+          isActive={isRecommendationItemActive(item)}
+          dataIndex={index}
+          trackInfo={item}
+          onClick={setCurrentTrack}
+          />
       })}
     </ul>
   )
-}
-
-const useRecommendations = () => {
-  const [recommendations, setRecommendations] = useState<TrackModel[]>([]);
-
-  useEffect(() => {
-    const randomTracks = getRecommendations();
-
-    setRecommendations(randomTracks);
-  }, [])
-
-  return recommendations;
 }
 
 export default RecommendationsList;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,36 @@
+import { RootState } from '@/app/store';
+import { TrackModel } from '@/interfaces/Track';
+import { getRecommendations } from '@/services/mockDataService';
 import RecommendationsList from "@features/Recommendations/components/RecommendationsList/RecommendationsList";
 import { Page } from "@interfaces/Page";
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import styles from './Home.module.scss';
 
 const Home = ({ title }: Page) => {
+  const recommendations = useRecommendations();
+  const trackList = useSelector((state: RootState) => state.tracks.playlist);
+
   return (
     <section className={styles.home}>
       <div className={`${styles.home__inner} _container`}>
         <h2 className={`${styles.home__pageTitle} _pageTitle`}>{title}</h2>
-        <RecommendationsList />
+        <RecommendationsList recommendationTracks={recommendations} trackList={trackList} />
       </div>
     </section>
   )
+}
+
+const useRecommendations = () => {
+  const [recommendations, setRecommendations] = useState<TrackModel[]>([]);
+
+  useEffect(() => {
+    const randomTracks = getRecommendations();
+
+    setRecommendations(randomTracks);
+  }, [])
+
+  return recommendations;
 }
 
 export default Home;
