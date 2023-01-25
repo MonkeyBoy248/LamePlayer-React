ded975f5980f5a7e4b926655887c1a6139ae177d
Kirill <kirik201100@mail.ru>
2023-01-25 16:48:27 +0300

    feat: add placeholder image for empty playlists
    LP-21

--- src/features/Playlists/components/Playlist/Playlist.tsx
+++ src/features/Playlists/components/Playlist/Playlist.tsx
@@ -1,28 +1,40 @@
-import React from "react";
 import styles from './Playlist.module.scss';
 import { Link } from 'react-router-dom';
 import { PlaylistModel } from "@interfaces/Playlist";
-import { TrackModel } from "@interfaces/Track";
 
 interface PlaylistProps {
   playlist: PlaylistModel;
 }
 
 const Playlist = ({ playlist }: PlaylistProps) => {
-  const getTracksAmount = (tracks: TrackModel[]) => {
-    const tracksAmount = tracks.length;
+  const getTracksAmount = () => {
+    const tracksAmount = playlist.tracks.length;
 
-    return tracksAmount > 1 ? `${tracksAmount} tracks` : `${tracksAmount} track` ;
+    return tracksAmount === 1 ? `${tracksAmount} track` : `${tracksAmount} tracks`;
+  }
+
+  const getPlaylistCover = () => {
+    if (playlist.coverUrl) {
+      return playlist.coverUrl;
+    }
+
+    const playlistTracksAmount = playlist.tracks.length;
+
+    if (playlistTracksAmount > 0) {
+      return playlist.tracks[playlistTracksAmount - 1].coverUrl;
+    }
+
+    return 'favorites-placeholder.jpg';
   }
 
   return (
     <li className={styles.playlist}>
       <Link className={styles.playlist__inner} to={`/playlist/${playlist.id}`}>
         <div className={styles.playlist__coverWrapper}>
-          <img className={styles.playlist__cover} src={`/images/covers/${playlist.coverUrl}`} alt={`${playlist.name} cover`} />
+          <img className={styles.playlist__cover} src={`/images/covers/${getPlaylistCover()}`} alt={`${playlist.name} cover`} />
         </div>
         <h3 className={`${styles.playlist__title} _itemTitle`}>{playlist.name}</h3>
-        <p className={styles.playlist__tracksAmount}>{getTracksAmount(playlist.tracks)}</p>
+        <p className={styles.playlist__tracksAmount}>{getTracksAmount()}</p>
       </Link>
     </li>
   )
