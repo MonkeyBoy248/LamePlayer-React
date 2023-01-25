c941ccc079db366eaa38e16e33b8f2a121f13901
Kirill <kirik201100@mail.ru>
2023-01-25 16:57:20 +0300

    feat: add favorites to the Playlists page
    LP-21

--- src/features/Playlists/components/PlayLists/PlayLists.tsx
+++ src/features/Playlists/components/PlayLists/PlayLists.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { PlaylistModel } from '@interfaces/Playlist';
 import Playlist from '../Playlist/Playlist';
 import styles from './PlayLists.module.scss';
@@ -6,15 +5,17 @@ import styles from './PlayLists.module.scss';
 
 interface PlaylistsProps {
   playlists: PlaylistModel[];
+  favorites: PlaylistModel;
 }
 
-const PlayLists = ({ playlists }: PlaylistsProps) => {
+const PlayLists = ({ playlists, favorites }: PlaylistsProps) => {
   return (
     <ul className={`${styles.playlists} _grid`}>
       <li className={styles.playlists__createPlaylist}>
         <button className={styles.playlists__createPlaylistButton}>+</button>
       </li>
-      { playlists.map((playlist) => {
+      <Playlist  playlist={favorites}/>
+      { playlists.length > 0 && playlists.map((playlist) => {
         return <Playlist key={playlist.id} playlist={playlist}/>
       }) }
     </ul>
