663a98094ca97fc1d96ac5a0be64a135438efa56
Kirill <kirik201100@mail.ru>
2023-01-29 22:56:52 +0300

    fix: save favorites to the local storage after deleting the last item in the playlist
    LP-21

--- src/features/Playlists/playlistsSlice.ts
+++ src/features/Playlists/playlistsSlice.ts
@@ -38,6 +38,8 @@ export const playlistsSlice = createSlice({
       if (favoritesTracksLength === 0) {
         state.favorites.coverUrl = 'favorites-placeholder.jpg';
 
+        setItemToLocalStorage('favorites', state.favorites)
+
         return;
       }
 
