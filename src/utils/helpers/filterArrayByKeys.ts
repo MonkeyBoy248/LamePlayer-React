b05ae6649e60b8ad3015b14b4b0039906821698e
Kirill <kirik201100@mail.ru>
2023-01-30 16:40:19 +0300

    refactor: create additional checks
    LP-21

--- src/utils/helpers/filterArrayByKeys.ts
+++ src/utils/helpers/filterArrayByKeys.ts
@@ -1,4 +1,12 @@
 export const filterArrayByKeys = <T>(array: T[], filters: (keyof T)[], value: string) => {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+
+  if (!array.length) {
+    return array;
+  }
+  
   return array.filter((item) => {
     return filters.some((filter) => (item[filter] as string).toLowerCase().includes(value.toLowerCase()));
   })
