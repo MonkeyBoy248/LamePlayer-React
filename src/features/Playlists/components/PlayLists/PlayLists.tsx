0e1e2e2eac373145c6e6d01fdd639be1e5f70cc3
Kirill <kirik201100@mail.ru>
2023-01-31 14:50:20 +0300

    feat: add logic for 'create new playlist' button
    LP-5

--- src/features/Playlists/components/PlayLists/PlayLists.tsx
+++ src/features/Playlists/components/PlayLists/PlayLists.tsx
@@ -1,4 +1,8 @@
+import { AppDispatch } from '@/app/store';
 import { PlaylistModel } from '@interfaces/Playlist';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createPlaylist } from '../../playlistsSlice';
 import PlaylistCard from '../PlaylistCard/PlaylistCard';
 import styles from './PlayLists.module.scss';
 
@@ -9,15 +13,34 @@ interface PlaylistsProps {
 }
 
 const PlayLists = ({ playlists, favorites }: PlaylistsProps) => {
+  const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const createNewPlaylist = () => {
+    const newPlaylist = {
+      id: crypto.randomUUID(),
+      name: 'New playlist',
+      dateOfCreation: Date.now(),
+      dateOfUpdate: Date.now(),
+      tracks: [],
+      coverUrl: 'playlist-placeholder.webp',
+      user: 'MonkeyBoy'
+    }
+
+    dispatch(createPlaylist(newPlaylist))
+    navigate(`/playlist/${newPlaylist.id}`)
+  }
+
   return (
     <ul className={`${styles.playlists} _grid`}>
       <li className={styles.playlists__createPlaylist}>
-        <button className={styles.playlists__createPlaylistButton}>+</button>
+        <button className={styles.playlists__createPlaylistButton} onClick={createNewPlaylist}>+</button>
       </li>
       <PlaylistCard  playlist={favorites}/>
       { playlists.length > 0 && playlists.map((playlist) => {
         return <PlaylistCard key={playlist.id} playlist={playlist}/>
-      }) }
+        })
+      }
     </ul>
   )
 }
