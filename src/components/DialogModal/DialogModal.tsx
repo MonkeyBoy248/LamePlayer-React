8de92572b52acc97d5254c4487a763dbc52c2c6e
Kirill <kirik201100@mail.ru>
2023-02-03 17:43:06 +0300

    refactor: change modal styles
    LP-4

--- src/components/DialogModal/DialogModal.tsx
+++ src/components/DialogModal/DialogModal.tsx
@@ -1,6 +1,7 @@
 import { iconIds } from '@/utils/config/iconIds';
 import { ReactNode } from 'react';
 import Icon from '../Icon';
+import { IconButton } from '../IconButton/IconButton';
 import { Modal } from '../Modal/Modal';
 import styles from './DialogModal.module.scss';
 
@@ -16,14 +17,14 @@ export const DialogModal = ({ isOpen, closeModal, children, title }: DialogModal
     <Modal isOpen={isOpen} closeModal={closeModal}>
       <header className={styles.dialog__header}>
         <h3 className={styles.dialog__title}>{title}</h3>
-        <button className={styles.dialog__closeButton}>
-          <Icon
-            id={iconIds.close}
+        <IconButton
+            iconId={iconIds.close}
             stroke={'#E5E5E5'}
-            width={'1rem'}
-            height={'1rem'}
-          />
-        </button>
+            width={'2rem'}
+            height={'2rem'}
+            className={styles.dialog__closeButton}
+            onClick={closeModal}
+        />
       </header>
       {children}
     </Modal>
