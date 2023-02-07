e5a997a483d21701bc61c04a394de244f610b17c
Kirill <kirik201100@mail.ru>
2023-02-07 15:11:20 +0300

    refactor: change the playlists slice structure
    replace 'favorites' property with 'favoritesId' to simplify work with the playlists and avoid creating two separate entities
    
    LP-26

--- src/pages/Playlists/PlaylistsPage.tsx
+++ src/pages/Playlists/PlaylistsPage.tsx
@@ -2,17 +2,16 @@ import PlayLists from "@features/Playlists/components/PlayLists/PlayLists";
 import { Page } from "@interfaces/Page";
 import styles from './PlaylistsPage.module.scss';
 import { useSelector } from 'react-redux';
-import { selectFavorites, selectCustomPlaylists } from '@/features/Playlists/selectors';
+import { selectAllPlaylists } from '@/features/Playlists/selectors';
 
 const PlaylistsPage = ({ title }: Page) => {
-  const favorites = useSelector(selectFavorites);
-  const playlists = Object.values(useSelector(selectCustomPlaylists));
+  const playlists = Object.values(useSelector(selectAllPlaylists));
 
   return (
     <section className={`${styles.playlistsPage} _page`}>
       <div className={`${styles.playlistsPage__inner} _container`}>
         <h2 className={`${styles.playlistsPage__title} _pageTitle`}>{title}</h2>
-        <PlayLists playlists={playlists} favorites={favorites}></PlayLists>
+        <PlayLists playlists={playlists}></PlayLists>
       </div>
     </section>
   )
