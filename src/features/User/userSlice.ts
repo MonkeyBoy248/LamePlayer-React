3bcd0ddd6f7bc728793e174c8f0a4986bfb10a29
Kirill <kirik201100@mail.ru>
2023-02-08 18:06:26 +0300

    feat: create slice for the data about the user
    LP-1

---
+++ src/features/User/userSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { getItemFromLocalStorage, setItemToLocalStorage } from "@utils/helpers/localStorage";
+
+export type Theme = 'light' | 'dark';
+
+export interface UserState {
+  theme: Theme;
+}
+
+const themeKey = 'theme';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: getInitialState(),
+  reducers: {
+    setTheme: (state, action: PayloadAction<Theme>) => {
+      state.theme = action.payload;
+
+      setItemToLocalStorage(themeKey, state.theme);
+    }
+  },
+})
+
+function getInitialState (): UserState {
+  const theme = getItemFromLocalStorage<Theme>(themeKey) ?? 'dark';
+
+  return {
+    theme
+  }
+}
+
+export const { setTheme } = userSlice.actions;
+export default userSlice.reducer;
