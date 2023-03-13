cf8d2598f9d62c21bdadfed1f1ae6d7ba1a0194c
Kirill Temnyi <kirik201100@mail.ru>
2023-03-13 17:38:15 +0300

    build: add Vitest dependency
    LP-46

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -9,4 +9,9 @@ export default defineConfig({
     open: true,
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+  },
 });
