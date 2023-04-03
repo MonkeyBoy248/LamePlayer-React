8d3358fd5e3a1ed6e60ca540ea96aa5ede9154a6
Kirill Temnyi <kirik201100@mail.ru>
2023-04-03 14:54:44 +0300

    refactor: add fallback for the title value
    LP-46

--- src/components/EmptyMessage/EmptyMessage.tsx
+++ src/components/EmptyMessage/EmptyMessage.tsx
@@ -15,7 +15,7 @@ export const EmptyMessage: FC<EmptyMessageProps> = ({ title, message }: EmptyMes
         <span className={styles.emptyMessage__icon}>
           <Icon id={iconIds.empty} width={'5em'} height={'5em'} fill={'#565656'} />
         </span>
-        <p className={styles.emptyMessage__title}>{title}</p>
+        <p className={styles.emptyMessage__title}>{title || 'Nothing found'}</p>
         {message ? <p className={styles.emptyMessage__text}>{message}</p> : null}
       </div>
     </div>
